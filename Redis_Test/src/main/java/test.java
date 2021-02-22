1da3f7503f559aa197d378f7271506855af877f3
xyc <xuyaochang@126.com>
2021-02-22 11:57:15 +0800

    将项目提交的本地仓库

---
+++ Redis_Test/src/main/java/test.java
@@ -0,0 +1,10 @@
+/**
+ * @author: ASD
+ * @date: 2021/2/22 11:52:34
+ * @description:
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("这是idea下的git测试");
+    }
+}
